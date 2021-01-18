7c3e3df97b3bbc0c648c6d03e01ca529108aba6a
Nikola <milje18@gmail.com>
2021-01-18 18:44:38 +0100

    display reactions

--- src/components/MessageBoard.js
+++ src/components/MessageBoard.js
@@ -2,8 +2,26 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import CreateReaction from './CreateReaction'
 
+const MessageReactions = ({ messageReactions }) => {
+    if(!messageReactions) return null;
+
+    return (
+        messageReactions.map((reaction,index) => {
+            const { id, emoji, username} = reaction;
+            return (
+                <span key={id}>
+                    <em>{username}:</em>
+                    {emoji}
+                    {index !== messageReactions.length - 1 ? ', ' : null}
+                </span>
+            )
+        })
+    )
+}
+
 const MessageBoard = () => {
     const messages = useSelector(state => state.messages)
+    const reactions = useSelector(state => state.reactions)
 
     return (
         <div>
@@ -16,6 +34,7 @@ const MessageBoard = () => {
                             <p>{text}</p>
                             <h4> - {username}</h4>
                             <CreateReaction messageId={id}/>
+                            <MessageReactions messageReactions= {reactions[id]} />
                             <hr/>
                         </div>
                     
